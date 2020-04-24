112e57082fc2b24b9254c2fdffdad98b5d76e005
watare <aurelien.watare@gmail.com>
2020-04-24 16:34:52 +0200

    correction bug sur le répertoire de travail

--- tools/lancement_pub_sub_sv.py
+++ tools/lancement_pub_sub_sv.py
@@ -15,18 +15,27 @@ for ied in config['ied']:
             print('dossier créé')
         except :
             print('erreur creation dossier')
-    
+
     run = dir +'/'+ied['id']
 
     #creation du fichier de run
 
     f= open(run+'/samplevalue.py','w')
     f.write("import subprocess as sub\n")
-    f.write("sub.run([\"/app/sv_publisher")
-    f.write(' '+ied['nic'] +' '+ ied['appid']+' '+'\''+ ied['logpath'] +'\''+' '+ied['iedname']+' '+str(ied['duration'])+'\"],shell=True)')
+
+    if ied['type'] =='publisher':
+        f.write("sub.run([\"/app/sv_publisher")
+        f.write(' '+ied['nic'] +' '+ ied['appid']+' '+'\''+ ied['logpath'] +'\''+' '+ied['iedname']+' '+str(ied['duration'])+'\"],shell=True)')
+    elif ied['type'] =='subscriber':
+        f.write("sub.run([\"/app/sv_subscriber")
+        f.write(' '+ied['nic'] +' '+ ied['appid']+' '+'\''+ ied['logpath'] +'\''+' '+str(ied['duration'])+'\"],shell=True)')
+
     f.close()
+    #lancement du conteneur
     client = docker.from_env()
     client.containers.run("watar/sv:latest",
     cap_add=["SYS_ADMIN","NET_ADMIN","NET_RAW","SYS_NICE"],
     detach=True,
+    remove=True,
+    network='macvlan',
     volumes={run:{'bind':'/run','mode':'rw'} })
