3fba999f9a586e0f5a298397a671bba16a654802
watare <aurelien.watare@gmail.com>
2020-04-24 13:07:29 +0200

    ajout d'un fichier de configuration toml permettant de définir les conteneurs a lancer generation automatique d un dossier par conteneur

--- tools/lancement_pub_sub_sv.py
+++ tools/lancement_pub_sub_sv.py
@@ -1,10 +1,32 @@
 import docker as docker
 import os
+import toml
 
+#identification du repertoire de travail
 dir = os.getcwd()
-run = dir + "/run"
-client = docker.from_env()
-client.containers.run("watar/sv:latest",
-cap_add=["SYS_ADMIN","NET_ADMIN","NET_RAW","SYS_NICE"],
-detach=True,
-volumes={run:{'bind':'/run','mode':'rw'} })
+
+#chargement du fichier de config
+config =  toml.load('conf.toml')
+for ied in config['ied']:
+    if not os.path.exists(ied['id']):
+        try:
+            #os.mkdir('dir/'+ied['id']+'/')
+            os.mkdir(ied['id'])
+            print('dossier créé')
+        except :
+            print('erreur creation dossier')
+    
+    run = dir +'/'+ied['id']
+
+    #creation du fichier de run
+
+    f= open(run+'/samplevalue.py','w')
+    f.write("import subprocess as sub\n")
+    f.write("sub.run([\"/app/sv_publisher")
+    f.write(' '+ied['nic'] +' '+ ied['appid']+' '+'\''+ ied['logpath'] +'\''+' '+ied['iedname']+' '+str(ied['duration'])+'\"],shell=True)')
+    f.close()
+    client = docker.from_env()
+    client.containers.run("watar/sv:latest",
+    cap_add=["SYS_ADMIN","NET_ADMIN","NET_RAW","SYS_NICE"],
+    detach=True,
+    volumes={run:{'bind':'/run','mode':'rw'} })
--- tools/run/samplevalue.py
+++ tools/run/samplevalue.py
@@ -7,4 +7,3 @@ sub.run(["/app/sv_publisher eth0 0x4000 '/run/log.csv' MU_POSTE_1 1"],shell=True
 f = open("dolbi.txt", "w")
 f.write("Woops! I have deleted the content!")
 f.close()
-
